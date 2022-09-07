81b1fab39f7c69d149ceb6eb4742049ac87e0105
RafiKurlansik <46289136+RafiKurlansik@users.noreply.github.com>
2022-09-07 15:44:14 -0400

    Update transforms_test.py

--- tests/transforms_test.py
+++ tests/transforms_test.py
@@ -35,7 +35,7 @@ def colnames_df() -> pd.DataFrame:
 # Make sure the filter works as expected.
 def test_filter(raw_input_df):
     filtered = filter_country(raw_input_df)
-    assert filtered.iso_code.drop_duplicates()[0] == "USA"
+    assert (filtered['entity'] == "United States").all()
 
 
 # The test data has NaNs for Daily ICU occupancy; this should get filled to 0.
